40e84d3568e09b75bdcf9a26b348236345ccb304
Ashley Noel Hinton <ahin017@aucklanduni.ac.nz>
2015-07-24 11:20:32 +1200

    Rpackages.R made sep file

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,11 +9,10 @@ echo deb http://cran.stat.auckland.ac.nz/bin/linux/ubuntu precise/ > \
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 # install packages
 # libxml2-dev is required by R package XML
-# libcurl4-openssl-dev is for RCurl
 # libxslt-dev and libxml2-dev for python lxml
 apt-get update
-apt-get install -y r-base r-base-dev libxml2-dev libcurl4-openssl-dev \
-    build-essential python-dev libxslt-dev python-matplotlib graphviz
+apt-get install -y r-base r-base-dev libxml2-dev build-essential python-dev \
+    libxslt-dev python-matplotlib graphviz
 
 ## customise /etc/matplotlibrc 
 ## sets backend to 'Agg' to prevent needing X windows
@@ -28,37 +27,5 @@ pip install -U numexpr
 pip install -U numpy
 pip install -U pandas
 
-## create R packages provisioning script -----------------------------------#
-cat > /tmp/Rpackages.R <<EOF
-## Create user package library location
-vagrantLib <- .Library.site[1]
-## select http://cran.stat.auckland.ac.nz for CRAN mirror
-chooseCRANmirror(
-    ind=which(getCRANmirrors()[,"URL"] == "http://cran.stat.auckland.ac.nz/"))
-## selecte CRAN, BioConductor, R-Forge for repos
-setRepositories(ind=c(1,2,8))
-
-## install packages from repos
-packages <- c("XML", "RCurl", "graph", "RBGL", "Rgraphviz", "gridGraphviz", 
-              "devtools")
-installIfRequired <- function(pkg) {
-    if (!require(package=pkg, character.only=TRUE)) {
-        install.packages(pkgs=pkg, lib=vagrantLib)
-    }
-}
-for (i in packages) {
-    installIfRequired(i)
-}
-while (!require("gridGraphviz")) {
-  install.packages("gridGraphviz", lib=vagrantLib)
-}
-
-## Install conduit
-if (!require(conduit)) {
-  devtools::install_github("anhinton/conduit", ref="develop", lib=vagrantLib)
-}
-
-EOF
-
 ## install R packages from /vagrant/Rpackages.R
-Rscript /tmp/Rpackages.R
+Rscript /vagrant/Rpackages.R
