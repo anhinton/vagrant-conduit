700f5dee17e43b672f864f9b31c61b92a627178b
Ashley Noel Hinton <ashley@canadia.co.nz>
2015-08-31 16:42:55 +1200

    fixed data.table requirement

--- Rpackages.R
+++ Rpackages.R
@@ -8,7 +8,7 @@ setRepositories(ind=c(1,2,8))
 
 ## install packages from repos
 packages <- c("RCurl", "XML", "graph", "RBGL", "Rgraphviz", "gridGraphviz", 
-              "devtools", "R2HTML", "ggplot2", "data.frame", "git2r",
+              "devtools", "R2HTML", "ggplot2", "data.table", "git2r",
               "lattice", "classInt")
 installIfRequired <- function(pkg) {
     if (!require(package=pkg, character.only=TRUE)) {
